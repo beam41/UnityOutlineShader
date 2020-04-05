6edc028e726d529790ccfac2812479baca402962
Meehoi <beam_baimon@hotmail.com>
2020-04-05 20:45:57 +0700

    add more interactive

---
+++ Assets/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject("Bicycle");
+                    _instance = container.AddComponent<GameManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    [Header("Shape")]
+    public GameObject torus;
+    public GameObject cube;
+    public GameObject teapot;
+    public GameObject stair;
+
+    [Header("Setting")]
+    public float tMovSpeed = 20f;
+
+    public void EnableTorus()
+    {
+        torus.SetActive(true);
+        cube.SetActive(false);
+        teapot.SetActive(false);
+        stair.SetActive(false);
+    }
+
+    public void EnableCube()
+    {
+        torus.SetActive(false);
+        cube.SetActive(true);
+        teapot.SetActive(false);
+        stair.SetActive(false);
+    }
+
+    public void EnableTeapot()
+    {
+        torus.SetActive(false);
+        cube.SetActive(false);
+        teapot.SetActive(true);
+        stair.SetActive(false);
+    }
+
+    public void EnableStair()
+    {
+        torus.SetActive(false);
+        cube.SetActive(false);
+        teapot.SetActive(false);
+        stair.SetActive(true);
+    }
+
+    public void changeSpeed(float value)
+    {
+        tMovSpeed = value;
+    }
+}
---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float rotSpeed = 1.5f;
+    private bool lastFrameMove = false;
+    private Vector3 lFMousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            if (lastFrameMove)
+            {
+                Vector3 deltaMousePos = lFMousePos - mousePos;
+                deltaMousePos = deltaMousePos * rotSpeed * Time.deltaTime;
+                transform.Rotate(deltaMousePos.y, -deltaMousePos.x, 0);
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+
+            }
+            lFMousePos = mousePos;
+            lastFrameMove = true;
+        }
+        else
+        {
+            lastFrameMove = false;
+        }
+        
+    }
+}
